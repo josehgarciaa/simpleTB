9faeb6bef62ac32c2cc0668e49e900c9a6c55228
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2024-02-20 17:13:00 +0100

    included the disordered graphene example dataset

---
+++ examples/disordered_graphene/disordered_graphene.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+lat = np.loadtxt("cell.dat").T # Vector are loaded in column form
+dims= 10,10, 1 
+
+#Metric matrix should be defined within the system once the vectors are loaded
+def Metric(lat): #This matrix is compatible with submiting the points in fractional coordinates as column vector
+    return ((lat.T)@(lat))
+metric_tensor = Metric(lat);
+
+frac_Apoints= np.array([x.flatten() for x in np.mgrid[0:dims[0], 0:dims[0], 0:1] ]) #Generates the lattice points in the fractional coordinates as column vectors
+frac_Bpoints= np.array([[1/3],[1/3],[0]])+frac_Apoints #Generates the lattice points in the fractional coordinates as column vectors
+frac_points = np.concatenate((frac_Apoints,frac_Bpoints), axis=1)
+cart_points = lat@frac_points
+
+import matplotlib.pyplot as plt
+
+plt.scatter(cart_points[0],cart_points[1])
+plt.title('Pristine Graphene')
+plt.xlabel('x (nm)')
+plt.ylabel('y (nm)')
+# Set aspect ratio to be equal
+plt.axis('equal')
+plt.savefig('pristinte_graphene.png')
+np.savetxt("pristinte_graphene.xyz", cart_points.T)
+
+
+#DISORDERED GRAPHENE
+frac_points = np.transpose([ (np.random.rand(3)-1)*0.2+x for x in frac_points.T])
+cart_points = lat@frac_points
+
+import matplotlib.pyplot as plt
+
+plt.scatter(cart_points[0],cart_points[1])
+plt.title('Disordered Graphene')
+plt.xlabel('x (nm)')
+plt.ylabel('y (nm)')
+# Set aspect ratio to be equal
+plt.axis('equal')
+plt.savefig('disordered_graphene.png')
+np.savetxt("disordered_graphene.xyz", cart_points.T)
+
+
+
+
+
+#from scipy.spatial.distance import cdist
+#a = 2.46;
+#a1 = np[1,0,0]
+
+#point_frac = [ x.flatten() for x in np.mgrid[0:5, 0:5, 0:1] ];
+#print(  [ x.flatten() for x in np.mgrid[0:5, 0:5, 0:1] ]   )
---
+++ graphene.py
@@ -0,0 +1,5 @@
+from simpletb.system import System
+
+
+
+wann_syst = System( dimensions = (100,100,1), w90_inp="linear_chain")
